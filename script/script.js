3ce00767c29d4e8351efa8965379081453b9c7a9
Maysson Ferro <mayssonalexandre@gmail.com>
2024-08-24 18:32:20 -0300

    so falta o search

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
-const storageForm = document.querySelector('#storage');
+const storageForm = document.querySelector('.storage');
 const storageInput = document.querySelector('#storageInput');
 const lista = document.querySelector('.lista-todo');
-const editStorage = document.querySelector('#editStorage');
+const editStorage = document.querySelector('.editStorage');
 const editInput = document.querySelector('#editInput');
 const cancelEdit = document.querySelector('#cancelEdit');
 
+let oldInputValue;
+
+
 //eventos
+
+//tirando a função de resetar a pagina depois do enviar
 storageForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    //tirando a função de resetar a pagina depois do enviar
     
     const inputValue = storageInput.value;
 
@@ -16,6 +20,53 @@ storageForm.addEventListener("submit", (event) => {
         saveTodo(inputValue)
     }
 });
+
+//marcar tarefa como concluida
+document.addEventListener("click", (evento) => {
+    
+    const targetEl = evento.target;
+    const parentEl = targetEl.closest(".todo");
+    let todoTitle;
+    
+    if(parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+    
+    if(targetEl.classList.contains("finish-todo")) {
+        console.log('finalizado');
+        parentEl.classList.toggle("done");
+    }
+    
+    if(targetEl.classList.contains("edit-todo")) {
+        toggleForms();
+        
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+    
+    if(targetEl.classList.contains("delete-todo")) {
+        parentEl.remove();
+    }
+})
+
+cancelEdit.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    toggleForms();
+});
+
+editStorage.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const editInputValue = editInput.value;
+    
+    if(editInputValue) {
+        updateTodo(editInputValue);
+    }
+
+    toggleForms()
+});
+
 
 //funções
 
@@ -54,5 +105,32 @@ const saveTodo = (text) => {
     deleteTodo.innerHTML = '<i class="bx bxs-trash-alt"></i>'
     todoButtons.appendChild(deleteTodo)
 
+    // salva tudo isso dentro da div lista
     lista.appendChild(todo);
-}
+
+    // limpa o input, para criar outra tarefa
+    storageInput.value = "";
+
+    // mantem o foco para o input
+    storageInput.focus();
+};
+
+//mostrar form para editar, e esconder irrelevantes
+const toggleForms = () => {
+    editStorage.classList.toggle("hide");
+    storageForm.classList.toggle("hide");
+    lista.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll('.todo');
+    
+    todos.forEach(todo => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    })
+};
